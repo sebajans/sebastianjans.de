d324c4db91d8487aa9288a39ccf7f32cdfa09d01
Sebastian Jans <sebajans@protonmail.com>
2022-12-08 13:09:31 +0100

    sebastianjans.de

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,17 @@ const config = {
 
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 };
 
-export default config;
+export default {
+  kit: {
+    adapter: adapter()
+  }
+};
+// export default config;
