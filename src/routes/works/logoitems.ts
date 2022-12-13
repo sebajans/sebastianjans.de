5c1868f75528c89137583fa78ee787ba32013bb7
Sebastian Jans <sebajans@protonmail.com>
2022-12-13 17:03:49 +0100

    changed path

--- src/routes/works/logoitems.ts
+++ src/routes/works/logoitems.ts
@@ -1,4 +1,4 @@
-import LogoChilli from '$assets/logos/logo-chilli.webp'
+import LogoChilli from '/logos/logo-chilli.webp'
 
 
 export const LogoItems = [
@@ -15,18 +15,18 @@ export const LogoItems = [
     class: `bg-primary-300 dark:bg-primary-600 shadow-primary-800/50 bg-[url('logos/logo-fachschaft.webp')] dark:bg-[url('logos/logo-fachschaft-white.webp')]`
   },
   {
-    class: ` right-[70%] bg-primary-400 dark:bg-primary-400 shadow-primary-800/50 bg-[url('logos/logo-sisimi.webp')] dark:bg-[url('logos/logo-sisimi-white.webp')]`
+    class: `bg-primary-400 dark:bg-primary-400 shadow-primary-800/50 bg-[url('logos/logo-sisimi.webp')] dark:bg-[url('logos/logo-sisimi-white.webp')]`
   },
   {
-    class: ` bg-primary-200 dark:bg-primary-200 shadow-primary-800/50 bg-[url('logos/logo-smyle.webp')]`
+    class: `bg-primary-200 dark:bg-primary-200 shadow-primary-800/50 bg-[url('logos/logo-smyle.webp')]`
   },
   {
-    class: ` bg-primary-400 dark:bg-primary-500 shadow-primary-800/50 bg-[url('logos/logo-ht.webp')] dark:bg-[url('logos/logo-ht-white.webp')]`
+    class: `bg-primary-400 dark:bg-primary-500 shadow-primary-800/50 bg-[url('logos/logo-ht.webp')] dark:bg-[url('logos/logo-ht-white.webp')]`
   },
   {
-    class: ` bg-primary-200 dark:bg-primary-600 shadow-primary-800/50 bg-[url('logos/logo-ys.webp')] dark:bg-[url('logos/logo-ys-white.webp')]`
+    class: `bg-primary-200 dark:bg-primary-600 shadow-primary-800/50 bg-[url('logos/logo-ys.webp')] dark:bg-[url('logos/logo-ys-white.webp')]`
   },
   {
-    class: ` bg-primary-300 dark:bg-primary-800 shadow-primary-800/50 bg-[url('logos/logo-cupholder.webp')] dark:bg-[url('logos/logo-cupholder-white.webp')]`
+    class: `bg-primary-300 dark:bg-primary-800 shadow-primary-800/50 bg-[url('logos/logo-cupholder.webp')] dark:bg-[url('logos/logo-cupholder-white.webp')]`
   }
 ];
