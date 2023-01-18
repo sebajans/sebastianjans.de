e39335342162c9104445a3f8a4be59a34bfa68ae
Sebastian Jans <sebajans@protonmail.com>
2023-01-18 20:27:55 +0100

    added jsons, fixed logo

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
 	import NavButton from './NavButton.svelte';
 	import { navItems } from './navItems';
 	import { onMount } from 'svelte';
+	import { beforeUpdate } from 'svelte';
+
 	import MenuToggle from './MenuToggle.svelte';
 	import BackdropBlur from './BackdropBlur.svelte';
 	import SocialMenu from '../components/SocialMenu.svelte';
@@ -16,7 +18,40 @@
 		showMenu = !showMenu;
 	}
 	$: languageStore;
+	
+	export let offset = 100;
+	export let tolerance = 3;
+	$: showMenu;
+	let headerClass: boolean = false;
+	let y = 0;
+	let lastY = 0;
+
+	function updateClass(y:number) {
+		const dy = lastY - y;
+		lastY = y;
+		return deriveClass(y, dy);
+	}
+	function deriveClass(y:number, dy:number) {
+		// show if at top of page
+		if (y < offset) {
+			return (headerClass = true);
+		}
+		// don't change state unless scroll is above threshold
+		if (Math.abs(dy) <= tolerance) {
+			headerClass != headerClass;
+			return headerClass;
+		}
+		if (dy < 0) {
+			headerClass = false;
+			return headerClass;
+		}
+		return (headerClass = true);
+	}
+
+	$: headerClass = updateClass(y);
+
 </script>
+<svelte:window bind:scrollY={y} />
 
 <header
 	class="{showMenu ? 'h-screen h-screen-ios' : 'h-0 md:h-screen md:h-screen-ios'} transition-transform md:w-40 z-30 fixed inset-y-0 flex justify-between flex-col w-full"
@@ -24,7 +59,10 @@
 	<BackdropBlur {showMenu} />
 	<!-- LOGO -->
 	<a aria-label="Back to Home" href="/" 
-		class="{showMenu ? 'mt-8 md:mt-4 scale-125 md:scale-100 mr-[0%]' : 'translate-x-1/2 mr-[100%]'} ease-out group hover:translate-y-2  md:w-20 w-16 md:h-20 h-16 m-4 md:my-4 md:mx-8 self-center relative transition-all duration-200 md:translate-x-0 z-50 group">
+		class="{headerClass || showMenu 
+			? ''
+			: 'opacity-0 md:opacity-100'}
+		{showMenu ? 'mt-8 md:mt-4 scale-125 md:scale-100 mr-[0%]' : 'translate-x-1/2 mr-[100%]'} ease-out group hover:translate-y-2 md:w-20 w-16 md:h-20 h-16 m-4 md:my-4 md:mx-8 self-center relative transition-all duration-200 md:translate-x-0 z-50 group">
 		<!-- <span class="w-[3px] group-hover:opacity-100 opacity-0 transition-all duration-200 -translate-y-[78px] translate-x-3 left-0 h-20 bg-primary-900 rounded absolute"></span> -->
 		<Logo className={''} />
 	</a>
