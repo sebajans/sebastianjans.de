8a2ccfb6951bdd0dcf324c2afc7622f8c4c71f53
sebajans <59985296+sebajans@users.noreply.github.com>
2023-03-09 11:25:01 +0100

    potential fix?

--- svelte.config.js
+++ svelte.config.js
@@ -12,9 +12,9 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		inlineStyleThreshold: Infinity,
-		output: {
-			dir: 'build'
-		},
+		// output: {
+		// 	dir: 'build'
+		// },
 		target: '#svelte',
 	},
 
