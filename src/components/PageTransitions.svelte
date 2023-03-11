3ce3e268518a0ba3272657d3d4350554b8a8cab1
sebajans <59985296+sebajans@users.noreply.github.com>
2023-03-11 22:30:44 +0100

    testing transition only on desktop

--- src/components/PageTransitions.svelte
+++ src/components/PageTransitions.svelte
@@ -1,15 +1,30 @@
 <script>
 	import { fly } from 'svelte/transition';
 	export let pathname = '';
+
+	let isMobile = false;
+	if (typeof window !== 'undefined') {
+		isMobile = window.innerWidth < 640;
+	}
 </script>
 
 {#key pathname}
-	<div
-		class="z-[3] overflow-x-clip w-full flex justify-center"
-		style="grid-row: 1 / -1; grid-column: 1 / -1;"
-		in:fly={{ x: 100, duration: 350, delay: 250 }}
-		out:fly={{ x: 100, duration: 350 }}
-	>
-		<slot />
-	</div>
+	{#if isMobile}
+		<!-- content here -->
+		<div
+			class="z-[3] overflow-x-clip w-full flex justify-center"
+			style="grid-row: 1 / -1; grid-column: 1 / -1;"
+			in:fly={{ x: 100, duration: 350, delay: 250 }}
+			out:fly={{ x: 100, duration: 350 }}
+		>
+			<slot />
+		</div>
+	{:else}
+		<div
+			class="z-[3] overflow-x-clip w-full flex justify-center"
+			style="grid-row: 1 / -1; grid-column: 1 / -1;"
+		>
+			<slot />
+		</div>
+	{/if}
 {/key}
